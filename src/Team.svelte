988696adf03d679b1fb91c4d3e320cae99079eab
David Lopez <dabraloma@outlook.com>
2023-07-27 11:45:49 -0700

    first commit

---
+++ src/Team.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let team;      
+    export let score;
+    export let gameOver;  
+
+    function add(){
+        score += 1;
+    }
+
+    function minus(){
+        score -= 1;
+    }
+
+</script>
+
+<div style="border: dashed {team.toLowerCase()} 5px;" class="col">
+    <h2 class:text-danger={team === 'Red'} class:text-primary={team === 'Blue'}>Team {team}: {score}</h2>
+    <button class="btn btn-success" on:click={add} disabled={gameOver}>+</button>
+    <button class="btn btn-danger" on:click={minus} disabled={gameOver}>-</button>
+</div>
+
+<style>
+    div.col{
+        padding: 20px;
+        text-align: center;
+    }
+</style>
